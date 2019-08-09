deb5025a30814190fba71e8fa6ebfe24f21c3a3d
PritamSangani <pritam.sangani@gmail.com>
2019-08-09 13:51:39 +0100

    [Pritam] [#1.1] Displayed welcome message to users when they first enter app

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,14 @@
 package com.twu.biblioteca;
 
 public class BibliotecaApp {
+    private static final String WELCOME_MESSAGE = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        BibliotecaApp app = new BibliotecaApp();
+        app.start();
+    }
+
+    public void start() {
+        System.out.println(WELCOME_MESSAGE);
     }
 }
